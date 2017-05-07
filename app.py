b9ffe12ee6c6523b4cc92219a9901990af04e8a9
Daniel Hall <halldn@gmail.com>
2017-05-07 16:29:42 -0700

    Add export feature and navbar

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 #app.py
 from flask import Flask, request, render_template, make_response
 import sqlite3
+import StringIO
+import csv
 
 app = Flask(__name__)
 app.config.from_object('config')
 # Now we can access the configuration variables via app.config["VAR_NAME"]
 
 dbfile = app.config["DBFILE"]
-
 
 @app.route("/")
 def index():
@@ -32,6 +33,9 @@ def index():
 		+",(SELECT b.Id FROM log_sensor b WHERE a.ModelNm = b.ModelNm AND a.HouseCd = b.HouseCd AND strftime('%Y-%m-%d', datetime(a.Timestamp, 'unixepoch', 'localtime')) = strftime('%Y-%m-%d', datetime(b.Timestamp, 'unixepoch', 'localtime')) ORDER BY b.TempC ASC, b.Timestamp ASC LIMIT 1) AS MinId "\
 		+",(SELECT b.Id FROM log_sensor b WHERE a.ModelNm = b.ModelNm AND a.HouseCd = b.HouseCd AND strftime('%Y-%m-%d', datetime(a.Timestamp, 'unixepoch', 'localtime')) = strftime('%Y-%m-%d', datetime(b.Timestamp, 'unixepoch', 'localtime')) ORDER BY b.TempC DESC, b.Timestamp DESC LIMIT 1) AS MaxId "\
 		+"FROM log_sensor a "\
+		+"WHERE 1 = 1 "\
+		+"AND strftime('%Y-%m-%d', datetime(a.Timestamp, 'unixepoch', 'localtime')) = '2017-04-25' "\
+		#+"AND strftime('%Y-%m-%d', datetime(a.Timestamp, 'unixepoch', 'localtime')) = strftime('%Y-%m-%d', datetime(strftime('%s', 'now'), 'unixepoch', 'localtime')) "\
 		+"GROUP BY 1, 2, 3 "\
 		+") grp "\
 		+"LEFT JOIN log_sensor lst "\
@@ -81,7 +85,8 @@ def hourly():
 			+",MIN(Id) AS MinId "\
 			+"FROM log_sensor a "\
 			+"WHERE 1 = 1 " \
-			#+"AND strftime('%Y-%m-%d', datetime(a.Timestamp, 'unixepoch', 'localtime')) >= datetime('2017-04-08', '-24 hour') "\
+			+"AND strftime('%Y-%m-%d', datetime(a.Timestamp, 'unixepoch', 'localtime')) = '2017-04-25' "\
+			#+"AND strftime('%Y-%m-%d', datetime(a.Timestamp, 'unixepoch', 'localtime')) = strftime('%Y-%m-%d', datetime(strftime('%s', 'now'), 'unixepoch', 'localtime')) "\
 			+"AND a.ModelNm || a.HouseCd = '" + sname + "' "\
 			+"GROUP BY "\
 			+"strftime('%Y-%m-%d', datetime(a.Timestamp, 'unixepoch', 'localtime')) "\
@@ -103,6 +108,54 @@ def hourly():
 		return render_template("hourly.html", HourlyLst = hourly_list, SensorNm = sname)
 	else:
 		return "Invalid querystring"
+
+@app.route("/export", methods=["GET"])
+def export():
+
+	# Connect to db
+	conn = sqlite3.connect(dbfile)
+	c = conn.cursor()
+	
+	exportDteNbr = request.args.get('d', None)
+	
+	# If there is no date in the args, display a table of export dates to choose from
+	if exportDteNbr == None:
+	
+		# Get list of report dates
+		c.execute("SELECT strftime('%Y-%m-%d', datetime(Timestamp, 'unixepoch', 'localtime')) AS Dte "\
+			+",strftime('%Y%m%d', datetime(Timestamp, 'unixepoch', 'localtime')) AS DteNbr "\
+			+",COUNT(*) AS RecordCt "\
+			+",MIN(strftime('%H:%M:%S',datetime(Timestamp, 'unixepoch', 'localtime'))) AS MinTime "\
+			+",MAX(strftime('%H:%M:%S',datetime(Timestamp, 'unixepoch', 'localtime'))) AS MaxTime "\
+			+"FROM log_sensor "\
+			+"GROUP BY strftime('%Y-%m-%d', datetime(Timestamp, 'unixepoch', 'localtime')) "\
+			+",strftime('%Y%m%d', datetime(Timestamp, 'unixepoch', 'localtime')) "\
+			+"ORDER BY Dte")
+	
+		# Convert results to dict
+		results = c.fetchall()
+		export_list = []
+		for row in results:
+			export_list.append({'Dte': row[0], 'DteNbr':row[1], 'RecordCt':row[2], 'MinTime':row[3], 'MaxTime':row[4]})
+
+		return render_template("export.html", ExportLst = export_list)
+	
+	# Otherwise, generate a csv file using date argument as a filter
+	else:
+		filename = 'RTL433Log-' + exportDteNbr + '.csv'
+		c.execute("""SELECT Id AS RecordNbr, ModelNm, HouseCd, ChannelCd, BatteryCd, TempC, 32+9.0/5*TempC AS TempF, HumidityPct, datetime(Timestamp, 'unixepoch', 'localtime') as Timestamp FROM log_sensor WHERE strftime('%Y%m%d', datetime(Timestamp, 'unixepoch', 'localtime')) = ? ORDER BY Id""", (exportDteNbr,))
+		
+		# Convert resultset to CSV
+		si = StringIO.StringIO()
+		cw = csv.writer(si)
+		cw.writerow([i[0] for i in c.description]) # heading row
+		cw.writerows(c.fetchall())
+		
+		# Encode CSV in response
+		output = make_response(si.getvalue())
+		output.headers["Content-Disposition"] = "attachment; filename=" + filename
+		output.headers["Content-type"] = "text/csv"
+		return output
 
 @app.route("/hello")
 def hello():
